9626f488f58c52cb94702f1ed828f3410401ad98
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-07-18 15:06:20 -0300

    Estilizando Step-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import StepForm1 from "./Forms/StepForm1";
+// import StepForm1 from "./Forms/StepForm1";
 // import StepForm2 from "./Forms/StepForm2";
 // import StepForm3 from "./Forms/StepForm3";
 // import Summary from "./Forms/Summary";
 function App() {
    return (
       <>
-      <StepForm1/>
+      {/* <StepForm1/> */}
       {/* <StepForm2/>
       <StepForm3/>
       <Summary/> */}
